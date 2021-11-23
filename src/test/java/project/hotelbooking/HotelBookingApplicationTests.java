c9d49394007895ec5af5a398ae011afcdd8f51f2
Miklos-design <73119146+Miklos-design@users.noreply.github.com>
2021-11-23 00:02:24 +0000

    initial commit

---
+++ src/test/java/project/hotelbooking/HotelBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package project.hotelbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
