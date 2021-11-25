be7b95c633bcbed4bc7a91ae288e7b22dd090e32
Miklos-design <73119146+Miklos-design@users.noreply.github.com>
2021-11-25 23:32:50 +0000

    Added test to  Room Services

--- src/test/java/project/hotelbooking/services/ReservationServiceTest.java
+++ src/test/java/project/hotelbooking/services/ReservationServiceTest.java
@@ -1,6 +1,6 @@
 package project.hotelbooking.services;
-
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -13,10 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import project.hotelbooking.crudrepo.GuestsRepo;
 import project.hotelbooking.crudrepo.ReservationRepo;
 import project.hotelbooking.entities.Reservation;
-import project.hotelbooking.service.GuestsService;
 import project.hotelbooking.service.ReservationService;
 
 
@@ -33,17 +31,17 @@ public class ReservationServiceTest {
 	void addReservTest() {
 		Reservation reservation = new Reservation();
 		when(repo.save(reservation)).thenReturn(reservation);
-		assertThat(reservation, service.addReservation(reservation));  
+		assertEquals( service.addReservation(reservation), reservation);  
 		verify(repo, times(1)).save(reservation);
 	}
 	
 	
-//	@Test
-//	void getAllGuests() {
-//		when(repo.findAll()).thenReturn(new ArrayList<>());
-//		assertThat(service.getAllGuests()).isEqualTo(new ArrayList<>());
-//		verify(repo, times(1)).findAll();
-//	}
+	@Test
+	void getAllResTest() {
+		when(repo.findAll()).thenReturn(new ArrayList<>());
+		assertThat(service.getReservations()).isEqualTo(new ArrayList<>());
+		verify(repo, times(1)).findAll();
+	}
 //	
 //	
 //	@Test
