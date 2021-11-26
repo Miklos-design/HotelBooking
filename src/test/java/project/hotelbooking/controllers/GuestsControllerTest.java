aea3cf7cd5ffa54acbc0a96702fd9ca37978341b
Miklos-design <73119146+Miklos-design@users.noreply.github.com>
2021-11-26 09:33:57 +0000

    Added tests

--- src/test/java/project/hotelbooking/controllers/GuestsControllerTest.java
+++ src/test/java/project/hotelbooking/controllers/GuestsControllerTest.java
@@ -1,7 +1,5 @@
 package project.hotelbooking.controllers;
 
-//import java.sql.Date;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
